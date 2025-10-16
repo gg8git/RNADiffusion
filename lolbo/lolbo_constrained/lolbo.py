77b1c71726b87beb5b6b1ac37022dfd03c4ecee6
gg8git <gauravgoel1248@gmail.com>
2025-10-16 13:11:48 -0400

    predictor initialization bug

--- lolbo/lolbo_constrained/lolbo.py
+++ lolbo/lolbo_constrained/lolbo.py
@@ -215,7 +215,9 @@ class LOLBOStateConstrained:
 
         if self.sample_extinct:
             predictor = ExtinctPredictor.load_from_checkpoint("./data/extinct_predictor.ckpt")
-            self.diffusion.predictor = predictor.cuda().freeze()
+            predictor.eval().cuda()
+            predictor.freeze()
+            self.diffusion.predictor = predictor
 
     def update_next(self, z_next_, y_next_, x_next_, c_next_=None, acquisition=False):
         """Add new points (z_next, y_next, x_next) to train data
